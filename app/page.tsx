72c67f501b159e2d3f54760f6ca5034b436a3087
juanpo12 <juanjoc2000@hotmail.com>
2025-11-17 00:24:57 -0300

    feat: add google maps embed for church location

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,20 @@ export default function Home() {
 
         ¡Es el tiempo donde la palabra  y el poder se unen para traer vida, sanidad y propósito!
       </p>
+      <div className="flex justify-center items-center w-full h-1/2">
+         <div className="aspect-video rounded-xl overflow-hidden shadow-lg">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.7526004077745!2d-58.56511212445445!3d-34.68619297292483!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc623af501339%3A0xc605a8a195d8ce69!2sAv.%20Brig.%20Gral.%20Juan%20Manuel%20de%20Rosas%204357%2C%20B1754FVB%20San%20Justo%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1757106329998!5m2!1ses-419!2sar"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="Ubicación Cristo La Solución"
+            />
+          </div>
+      </div>
       <Presenters />
       <TicketModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </main>
